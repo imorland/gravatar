cea9b5015829c409f1ba8dd52dcd57d303c94345
Ian Morland <ian@morland.me>
2021-06-29 21:21:06 +0100

    Use core help labels

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -5,52 +5,36 @@ import ratingOptions from '../common/utils/ratingOptions';
 app.initializers.add('ianm/gravatar', () => {
     app.extensionData
         .for('ianm-gravatar')
-        .registerSetting(function () {
-            return [
-                <div class="helpText">{app.translator.trans('ianm-gravatar.admin.settings.proxy.helptext')}</div>
-            ]
-        })
         .registerSetting({
             label: app.translator.trans('ianm-gravatar.admin.settings.proxy.title'),
             setting: 'ianm-gravatar.proxy',
             type: 'bool',
-        })
-        .registerSetting(function () {
-            return [<div class="helpText">{app.translator.trans('ianm-gravatar.admin.settings.replace-flarum-custom.helptext')}</div>];
+            help: app.translator.trans('ianm-gravatar.admin.settings.proxy.helptext'),
         })
         .registerSetting({
             label: app.translator.trans('ianm-gravatar.admin.settings.replace-flarum-custom.title'),
             setting: 'ianm-gravatar.replace-flarum-custom',
             type: 'bool',
-        })
-        .registerSetting(function () {
-            return [
-                <h3>{app.translator.trans('ianm-gravatar.admin.settings.defaults.title')}</h3>,
-                <div class="helpText">{app.translator.trans('ianm-gravatar.admin.settings.defaults.helptext')}</div>,
-            ];
+            help: app.translator.trans('ianm-gravatar.admin.settings.replace-flarum-custom.helptext'),
         })
         .registerSetting({
+            label: app.translator.trans('ianm-gravatar.admin.settings.defaults.title'),
             setting: 'ianm-gravatar.default',
             type: 'select',
             options: defaultOptions(),
-        })
-        .registerSetting(function () {
-            return [<div class="helpText">{app.translator.trans('ianm-gravatar.admin.settings.force-default.helptext')}</div>];
+            help: app.translator.trans('ianm-gravatar.admin.settings.defaults.helptext'),
         })
         .registerSetting({
             label: app.translator.trans('ianm-gravatar.admin.settings.force-default.title'),
             setting: 'ianm-gravatar.force-default',
             type: 'bool',
-        })
-        .registerSetting(function () {
-            return [
-                <h3>{app.translator.trans('ianm-gravatar.admin.settings.rating.title')}</h3>,
-                <div class="helpText">{app.translator.trans('ianm-gravatar.admin.settings.rating.helptext')}</div>,
-            ];
+            help: app.translator.trans('ianm-gravatar.admin.settings.force-default.helptext'),
         })
         .registerSetting({
+            label: app.translator.trans('ianm-gravatar.admin.settings.rating.title'),
             setting: 'ianm-gravatar.rating',
             type: 'select',
             options: ratingOptions(),
+            help: app.translator.trans('ianm-gravatar.admin.settings.rating.helptext'),
         });
 });
